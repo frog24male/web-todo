f8f93c45e49d9842e837160371ce354bfe5c9232
frog24male <frog24male@gmail.com>
2024-07-10 11:47:33 +0000

    addding add button

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import function
 
 todos=function.get_todos()
 
+def add_todo():
+    todo=st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    function.write_todos(todos)
+
 st.title("my Todo app")
 st.subheader("This is my To do app")
 
@@ -13,4 +18,5 @@ for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label="",placeholder="Enter a new todo")
+st.text_input(label="Add Todo",placeholder="Enter a new todo",
+              on_change=add_todo,key="new_todo")
