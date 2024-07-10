9e8a5e803b688ca52283e247c0550660881aa4f5
frog24male <frog24male@gmail.com>
2024-07-10 15:33:13 +0000

    c

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ todos=function.get_todos()
 
 def add_todo():
     todo=st.session_state["new_todo"]+"\n"
-    todos.append(todo)
+#del st.session_state[todo]
+    todos.append(todo) 
     function.write_todos(todos)
 
 st.title("my Todo app")
@@ -14,8 +15,14 @@ st.subheader("This is my To do app")
 
 st.write("This app is to increase your productivity")
 st.checkbox("Buy mac")
+
 for todo in todos:
-    st.checkbox(todo)
+    checkbox=st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.remove(todo)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 
 st.text_input(label="Add Todo",placeholder="Enter a new todo",
